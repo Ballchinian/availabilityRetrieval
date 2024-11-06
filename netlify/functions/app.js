de59023ae4f10749b7418482b1db65bef524bc86
ballchinian <ethantnewiss@hotmail.co.uk>
2024-11-06 12:17:41 +0000

    updated varable names for uri, database and collection

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -1,13 +1,13 @@
 const { MongoClient } = require("mongodb");
 
 async function getData() {
-  const uri = "your_mongo_connection_string";
+  const uri = process.env.ATLAS_URI;
   const client = new MongoClient(uri);
 
   try {
     await client.connect();
-    const database = client.db("your_database_name");
-    const collection = database.collection("your_collection_name");
+    const database = client.db('myDatabase');
+    const collection = database.collection("users");
 
     // Query to retrieve only the name and selectedDates fields
     const projection = { name: 1, selectedDates: 1, _id: 0 }; // Set _id to 0 to exclude it
